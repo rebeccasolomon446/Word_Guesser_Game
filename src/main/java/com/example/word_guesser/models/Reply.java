b3d4a9d29873e115ee637586501b2925855970ce
Your Name <rebecca.solomon446@gmail.com>
2023-07-18 14:56:46 +0100

    creates Reply model for client communication

---
+++ src/main/java/com/example/word_guesser/models/Reply.java
@@ -0,0 +1,33 @@
+package com.example.word_guesser.models;
+
+public class Reply {
+    private String wordState;
+    private String message;
+
+    public Reply(String wordState, String message) {
+        this.wordState = wordState;
+        this.message = message;
+    }
+
+    public Reply() {
+    }
+
+    // Getters and Setters
+
+
+    public String getWordState() {
+        return wordState;
+    }
+
+    public void setWordState(String wordState) {
+        this.wordState = wordState;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
